a318aa0ff5bb5ce5bac07fa698f5d0367dfc4a7c
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-04-05 12:59:23 -0400

    add support for changing the ratio i think

--- Pomorado.js
+++ Pomorado.js
@@ -1,18 +1,32 @@
+WORK = 25000 * 60;
+BREAK = 5000 * 60;
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
-const yourFunction = async () => {
+function GetConfs () {
+	var confs = document.forms["form"]["confs"].value;
+	if (confs == "5break") {
+		WORK = 25000 * 60;
+		BREAK = 5000 * 60;
+	}
+	else if (confs == "10break") {
+		WORK = 50000 * 60;
+		BREAK = 10000 * 60;
+	}
+}
+
+const yourFunction = async () => { //https://stackoverflow.com/a/47480429/9654083
 	for (;;) {
 	Notif1("Work time!","Pomotimer Alert: WORK!");
-  await delay(25000 * 60);
+  await delay(WORK);
   document.getElementById("bigbold").innerHTML = "Currently On: <b><u>BREAK</u></b>";
 Notif1("Break time!","Pomotimer alert: BREAK!");
-  await delay(5000 * 60);
+  await delay(BREAK);
 	Notif1("Work time!","Pomotimer alert: Work!");
   document.getElementById("bigbold").innerHTML = "Currently On: <b><u>WORK</u></b>";
+		document.getElementById("placeToRest").innerHTML = ""
 	}
 };
 function Notif1(title, body) {
-	console.log(1.1)
 icon = 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fsvgsilh.com%2Fsvg%2F26092.svg&imgrefurl=https%3A%2F%2Fsvgsilh.com%2Fimage%2F26092.html&tbnid=CSLXtdRhU_xYCM&vet=12ahUKEwiD-pC7q-fvAhUROawKHUlfD24QMygAegQIARBl..i&docid=CyW1JYWiQHh62M&w=1707&h=1707&q=timer%20cc0&ved=2ahUKEwiD-pC7q-fvAhUROawKHUlfD24QMygAegQIARBl'; //https://levelup.gitconnected.com/creating-browser-notification-in-javascript-79e91bfb76c8
-console.log(1.2);var notification = new Notification(title, { body, icon });console.log(1.3)
+var notification = new Notification(title, { body, icon })
 }
