5aa9b2621b09628f74557d6332c66baf00d8dcf8
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-06-05 12:20:51 -0400

    fix bug! my mind was set on that one solution that i didn't think of this

--- Pomorado.js
+++ Pomorado.js
@@ -1,36 +1,34 @@
-WORK = 25000 / 2;
-BREAK = 5000 / 2;
-STOP = 0;
-times = 120;
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 function GetConfs () {
-	var confs = document.forms["form"]["confs"].value;
+	const queryString = window.location.search;
+console.log("queryString will follow")
+console.log(queryString);
+const urlParams = new URLSearchParams(queryString);
+const confs = urlParams.get('confs');
 	if (confs == "5break") {
-		WORK = 25000 / 2;
-		BREAK = 5000 / 2;
+        WORK = 1500000;
+        //WORK = 1000; //i was testing
+		BREAK = 300000;
 	}
 	else if (confs == "10break") {
-		WORK = 50000 / 2;
-		BREAK = 10000 / 2;
+		WORK = 3000000;
+		BREAK = 600000;
 	}
-	STOP = 1;
 }
 
 const yourFunction = async () => { //https://stackoverflow.com/a/47480429/9654083
 	for (;;) {
-	Notif1("Work time!","Pomotimer Alert: WORK!");i=1;
+        GetConfs()
+	Notif1("Work time!","Pomotimer Alert: WORK!")
         console.log(WORK);
         console.log(BREAK);
-        while (i >= 120) { console.log(i);console.log(STOP);if (STOP == 1) { throw new Error("Confs updated; stopping..."); } else if (STOP == 0) { await delay(WORK);i++}}
+        await delay(WORK);
   document.getElementById("bigbold").innerHTML = "Currently On: <b><u>BREAK</u></b>";
 Notif1("Break time!","Pomotimer alert: BREAK!");
-        i = 1;
-        while (i >= 120) { if (STOP == 1) { throw new Exception("Confs updated; stopping...");} await delay(BREAK); } i++;//https://stackoverflow.com/a/10993837/9654083
-  document.getElementById("bigbold").innerHTML = "Currently On: <b><u>WORK</u></b>";
-		document.getElementById("placeToRest").innerHTML = ""
-	}
-};
+        await delay(BREAK);
+document.getElementById("bigbold").innerHTML = "Currently On: <b><u>WORK</u></b>";
+    }}
 function Notif1(title, body) {
 icon = 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fsvgsilh.com%2Fsvg%2F26092.svg&imgrefurl=https%3A%2F%2Fsvgsilh.com%2Fimage%2F26092.html&tbnid=CSLXtdRhU_xYCM&vet=12ahUKEwiD-pC7q-fvAhUROawKHUlfD24QMygAegQIARBl..i&docid=CyW1JYWiQHh62M&w=1707&h=1707&q=timer%20cc0&ved=2ahUKEwiD-pC7q-fvAhUROawKHUlfD24QMygAegQIARBl'; //https://levelup.gitconnected.com/creating-browser-notification-in-javascript-79e91bfb76c8
 var notification = new Notification(title, { body, icon })
